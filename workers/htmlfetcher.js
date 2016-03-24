76aea890a08650ad78c8e0d2cbcd2d9628e8d8dc
ranebo <ranegri3@gmail.com>
2016-03-24 12:25:34 -0700

    Add comment with cron instructions

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,5 +1,11 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+// crontab instructions
+// crontab -e
+// add to file: */1 * * * * /usr/local/bin/node ~/desktop/2016-02-web-historian/workers/htmlfetcher.js
+// crontab -l to view existing crons
+
 var archive = require('../helpers/archive-helpers.js');
 
 archive.readListOfUrls(archive.downloadUrls);
